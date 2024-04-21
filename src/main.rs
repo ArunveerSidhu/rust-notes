f52ac5afb2bfc84d7371351a6fad9e0d58b10eb4
Arunveer Sidhu <sidhu.arunveer@gmail.com>
2024-04-21 16:11:02 +0530

    till enums

--- src/main.rs
+++ src/main.rs
@@ -79,4 +79,71 @@ fn main() {
     println!("balance: {}", v3);
 
 
+    //string
+    
+    // let mut st1:String = String::new();
+    // st1.push('A');
+    // st1.push_str(" word");
+    // for word in st1.split_whitespace(){
+    //     println!("{}", word);
+    // }
+    // let st2 = st1.replace('A', "Another");
+    // println!("{}", st2);
+    
+    let st3 = String::from("x r t b h k k a m c ");
+    let mut v1: Vec<char> = st3.chars().collect();
+    v1.sort();
+    v1.dedup(); 
+
+    for char in v1{
+        println!("{}", char);
+    }
+
+    let st4: &str = "Random String";
+    let mut st5 = st4.to_string();
+    println!("{}", st5);
+    let byte_arr1: &[u8] = st5.as_bytes();
+    let st6 = &st5[0..6];
+    println!("String Length: {}", st6.len());
+    st5.clear();
+
+    let st6 = String::from("Just Some");
+    let st7 = String::from(" Words");
+    let st8 = st6 + &st7;
+    for char in st8.bytes(){
+        println!("{}", char);
+    } 
+
+    // enums
+
+    enum Day {
+        Monday,
+        Teusday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+
+    impl Day {
+        fn is_weekend(&self) -> bool{
+            match self {
+                Day::Saturday | Day::Sunday => true, _ => false
+            }
+        }
+    }
+    let today:Day = Day::Monday;
+    match today {
+        Day::Monday => println!("naah"),
+        Day::Teusday => println!("na"),
+        Day::Wednesday => println!("yeah"),
+        Day::Thursday => println!("hh"),
+        Day::Friday => println!("xx"),
+        Day::Saturday => println!("Weekend"),
+        Day::Sunday => println!("Weekend"),
+    }
+
+    println!("Is today the weekend {}", today.is_weekend());
+
 }
